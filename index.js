2cf942960b99856367701365c8379af848464b1b
chomba <chomba@bongohive.co.zm>
2022-01-24 13:41:43 +0200

    add lowdb cors and morgan

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
 
+const morgan = require("morgan");
 
-app.get('/', (req, res) => {
-    res.send("Lets dowit!!")
-})
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("server connected at port 5000")
-})
+app.use(cors());
+app.use(express());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Lets dowit!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server connected at port ${PORT}`);
+});
